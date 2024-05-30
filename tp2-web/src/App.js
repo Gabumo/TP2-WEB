7b123f3fa68f81a41a48f1d51b206927b596beac
Francis Dubois <francisdubois28@gmail.com>
2024-05-29 20:05:34 -0400

    Ajout authorisation avec route privee

--- tp2-web/src/App.js
+++ tp2-web/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useAuth0 } from '@auth0/auth0-react';
 import { BarreNavigation } from './BarreNavigation.js';
 import {PageAccueil} from './pages/PageAccueil.js';
 import { PageListeClients } from './pages/PageListeClients.js';
@@ -7,24 +8,49 @@ import { PageAjoutClient } from './pages/PageAjoutClient.js';
 import { PageModificationClient } from './pages/PageModificationClient.js';
 import { PageModificationAjoutAdresse } from './pages/PageModificationAjoutAdresse.js';
 import { PageConfirmationSuppression } from './pages/PageConfirmationSuppression.js';
+import { RoutePrivee } from './RoutePrivee.js';
+
 import { 
   BrowserRouter,
   Routes,
-  Route
+  Route,
+  Link
 } from 'react-router-dom';
 
 function App() {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
   return (
     <>
     <BrowserRouter>
+    <div>
+        <ul>
+          {isAuthenticated ? (
+            <>
+              <button onClick={() => logout()}>Se déconnecter</button>
+              <li>
+                <Link to="/">Page d'accueil</Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <button onClick={() => loginWithRedirect()}>Se connecter</button>
+              <li>
+                <Link to="/">Page d'accueil</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
     <BarreNavigation />
       <Routes>
         <Route path="/" element={<PageAccueil />} />
+        <Route element={<RoutePrivee />}>
         <Route path="/ajout-client" element={<PageAjoutClient />} />
         <Route path="/liste-clients" element={<PageListeClients />} />
         <Route path="/modification/:id" element={<PageModificationClient />} />
         <Route path="/modification/:id/:adresseId" element={<PageModificationAjoutAdresse />} />
         <Route path="/confirmation-suppression/:id" element={<PageConfirmationSuppression />} />
+        </Route>
       </Routes>
     </BrowserRouter>
     </>
---
+++ tp2-web/src/RoutePrivee.js
@@ -0,0 +1,15 @@
+import { Outlet, Navigate } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+
+export const RoutePrivee = () => {
+    const { isAuthenticated, isLoading, error } = useAuth0();
+
+    if(isLoading){
+        return <div>Chargement en cours...</div>;
+    }
+    if(error){
+        return <div>Erreur: {error.message}</div>;
+    }
+
+    return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
+    };
